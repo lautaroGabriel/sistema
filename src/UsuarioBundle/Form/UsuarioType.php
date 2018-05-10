79f3757d0271eab0f3b0427d661b4914cfe15595
lautaro gonzalez <lautarogabrielgonzalez1@gmail.com>
2018-05-10 10:42:22 -0300

    creacion de una base de datos,entidad y formulario

---
+++ src/UsuarioBundle/Form/UsuarioType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace UsuarioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('password')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UsuarioBundle\Entity\Usuario'
+        ));
+    }
+}
